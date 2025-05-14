4d490423e1efbaf68524da03c592eaf525a7e8c3
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-05-14 13:49:58 +0100

    Add layout with sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
+
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+      <html lang="en">
       <body>{children}</body>
-    </html>
+      <body className={font.className}>{children}</body>
+      </html>
   );
 }
