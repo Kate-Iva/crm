05fe49edd84ed1298a354cb4b2f224d92cf90de1
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-06-27 00:07:52 +0100

    Install react-query

--- src/app/components/providers.tsx
+++ src/app/components/providers.tsx
@@ -2,7 +2,7 @@
 
 import React, { useMemo } from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-//import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function Providers({ children }: React.PropsWithChildren) {
     const client = useMemo(() => new QueryClient(), []);
@@ -10,7 +10,7 @@ export default function Providers({ children }: React.PropsWithChildren) {
     return (
         <QueryClientProvider client={client}>
             {children}
-
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
 }
