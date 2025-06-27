3cbb5e8de9f75a54012ee86048699bf8cf953c29
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-06-27 23:10:06 +0100

    Add using react-query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import AddCompanyButton from '@/app/components/add-company-button';
-
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
-      <AddCompanyButton />
     </main>
   );
 }
